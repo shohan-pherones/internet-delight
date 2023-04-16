8291b1b740c689fc3bca748fb98078f9eb89bdce
shohan-pherones <shohan.sub.56@gmail.com>
2023-04-16 13:44:07 +0600

    blog updated

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,11 +1,5 @@
-import React from "react";
-import Link from "next/link";
-import { format } from "date-fns";
-import { MdOutlineBookmarkAdd } from "react-icons/md";
-import { AiOutlineMinusCircle } from "react-icons/ai";
-import { FiMoreHorizontal } from "react-icons/fi";
 import { getAllBlogs } from "@/prisma/blogs";
-import Image from "next/image";
+import Blog from "@/components/Blog";
 
 const BlogsPage = ({ blogs }) => {
   return (
@@ -14,77 +8,9 @@ const BlogsPage = ({ blogs }) => {
         <div className="blog-header py-5">
           <h2 className="text-2xl">Latest Posts</h2>
         </div>
-        <div className="blog-content">
+        <div className="blog-content flex flex-col gap-5">
           {blogs.length > 0 &&
-            blogs.map((blog) => (
-              <div
-                key={blog.id}
-                className="border-b-2 border-gray-200 grid grid-cols-12 gap-5 lg:gap-20 pb-10"
-              >
-                <div className="flex flex-col col-span-8 row-span-full gap-5">
-                  <div className="flex justify-start">
-                    <div className="inline-flex items-center gap-2 text-sm text-gray-900 dark:text-white">
-                      <Image
-                        src={blog.authorImage}
-                        alt={blog.authorName}
-                        width={40}
-                        height={40}
-                        className="rounded-full"
-                      />
-                      <div className="flex items-center gap-5">
-                        <Link
-                          href="/profile"
-                          rel="author"
-                          className="font-bold text-gray-900"
-                        >
-                          {blog.authorName}
-                        </Link>
-                        <span className="font-medium text-gray-600">
-                          {blog.createdAt}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <Link href={blog.id}>
-                    <h1 className="text-md lg:text-4xl font-semibold">
-                      {blog.title}
-                    </h1>
-                  </Link>
-                  <p className="line-clamp-2 lg:line-clamp-3">{blog.body}</p>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-5">
-                      <Link
-                        href="/tags"
-                        className="bg-gray-200 text-black rounded-full py-1 px-4"
-                      >
-                        {blog.tags.split(",").at(0)}
-                      </Link>
-
-                      <span>6 min read</span>
-                    </div>
-                    <div className="text-xl text-gray-500 flex items-center gap-5">
-                      <button>
-                        <MdOutlineBookmarkAdd />
-                      </button>
-                      <button>
-                        <AiOutlineMinusCircle />
-                      </button>
-                      <Link href="/">
-                        <FiMoreHorizontal />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-span-4 flex items-center justify-end">
-                  <Image
-                    src={blog.cover}
-                    alt={blog.title}
-                    width={400}
-                    height={400}
-                  />
-                </div>
-              </div>
-            ))}
+            blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
         </div>
       </div>
     </div>
