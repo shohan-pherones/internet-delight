398ba364d6def645f6f2d379ce97dfd398410dba
sakhyat <sakhyatkabir1@gmail.com>
2023-05-01 06:44:36 -0400

    post form init

---
+++ pages/blogs/create.jsx
@@ -0,0 +1,15 @@
+import PostForm from "@/components/PostForm";
+import React from "react";
+
+const create = () => {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <h1 className="text-3xl my-3">What do you think now!</h1>
+      <div className="post-wrapper">
+        <PostForm />
+      </div>
+    </div>
+  );
+};
+
+export default create;
